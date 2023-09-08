5f1ed055d0839163efcfdbe2cd7050482ff906c9
scott xue <scottxue@berkeley.edu>
2023-09-08 15:41:17 -0700

    delete the tsx

--- public/assets/tilemaps/victoria_ashford_internal.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="victoria_ashford_internal" tilewidth="64" tileheight="64" tilecount="273" columns="13">
- <image source="../../../base_assets/victoria_ashford_internal.png" width="832" height="1344"/>
-</tileset>
